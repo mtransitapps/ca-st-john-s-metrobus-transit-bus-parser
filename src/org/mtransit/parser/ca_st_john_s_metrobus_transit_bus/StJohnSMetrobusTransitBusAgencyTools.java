5fa06cd5d233e8ca2d2515a237a5df13ef6a8873
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:31:59 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_st_john_s_metrobus_transit_bus/StJohnSMetrobusTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_st_john_s_metrobus_transit_bus/StJohnSMetrobusTransitBusAgencyTools.java
@@ -35,7 +35,7 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Metrobus Transit bus data...\n");
+		System.out.printf("\nGenerating Metrobus Transit bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
