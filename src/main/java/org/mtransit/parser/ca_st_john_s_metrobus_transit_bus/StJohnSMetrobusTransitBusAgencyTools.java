c730e91d3adb609113d895496cdaf5f530fc47f1
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-27 18:24:34 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_st_john_s_metrobus_transit_bus/StJohnSMetrobusTransitBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_st_john_s_metrobus_transit_bus/StJohnSMetrobusTransitBusAgencyTools.java
@@ -1,83 +1,47 @@
 package org.mtransit.parser.ca_st_john_s_metrobus_transit_bus;
 
+import static org.mtransit.commons.RegexUtils.DIGITS;
+import static org.mtransit.commons.StringUtils.EMPTY;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.mtransit.parser.CleanUtils;
+import org.mtransit.commons.CharUtils;
+import org.mtransit.commons.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.StringUtils;
-import org.mtransit.parser.Utils;
-import org.mtransit.parser.gtfs.data.GCalendar;
-import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
-import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
-import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.mtransit.parser.StringUtils.EMPTY;
 
 // https://www.metrobus.com/html-default/gtfs.asp
 // https://www.metrobus.com/html-default/documents.asp
 // https://www.metrobus.com/google/google_transit.zip
 public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 
-	public static void main(@Nullable String[] args) {
-		if (args == null || args.length == 0) {
-			args = new String[3];
-			args[0] = "input/gtfs.zip";
-			args[1] = "../../mtransitapps/ca-st-john-s-metrobus-transit-bus-android/res/raw/";
-			args[2] = ""; // files-prefix
-		}
+	public static void main(@NotNull String[] args) {
 		new StJohnSMetrobusTransitBusAgencyTools().start(args);
 	}
 
 	@Nullable
-	private HashSet<Integer> serviceIdInts;
-
 	@Override
-	public void start(@NotNull String[] args) {
-		MTLog.log("Generating Metrobus Transit bus data...");
-		long start = System.currentTimeMillis();
-		this.serviceIdInts = extractUsefulServiceIdInts(args, this, true);
-		super.start(args);
-		MTLog.log("Generating Metrobus Transit bus data... DONE in %s.", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	public List<Locale> getSupportedLanguages() {
+		return LANG_EN;
 	}
 
 	@Override
-	public boolean excludingAll() {
-		return this.serviceIdInts != null && this.serviceIdInts.isEmpty();
+	public boolean defaultExcludeEnabled() {
+		return true;
 	}
 
+	@NotNull
 	@Override
-	public boolean excludeCalendar(@NotNull GCalendar gCalendar) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarInt(gCalendar, this.serviceIdInts);
-		}
-		return super.excludeCalendar(gCalendar);
-	}
-
-	@Override
-	public boolean excludeCalendarDate(@NotNull GCalendarDate gCalendarDates) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessCalendarDateInt(gCalendarDates, this.serviceIdInts);
-		}
-		return super.excludeCalendarDate(gCalendarDates);
-	}
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (this.serviceIdInts != null) {
-			return excludeUselessTripInt(gTrip, this.serviceIdInts);
-		}
-		return super.excludeTrip(gTrip);
+	public String getAgencyName() {
+		return "Metrobus Transit";
 	}
 
 	@NotNull
@@ -86,39 +50,31 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
-	private static final long RID_ENDS_WITH_A = 10_000L;
-	private static final long RID_ENDS_WITH_B = 20_000L;
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
 
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		String rsn = gRoute.getRouteShortName().trim();
-		if (!Utils.isDigitsOnly(rsn)) {
-			Matcher matcher = DIGITS.matcher(rsn);
-			if (matcher.find()) {
-				int digits = Integer.parseInt(matcher.group());
-				if (rsn.endsWith("A")) {
-					return digits + RID_ENDS_WITH_A;
-				} else if (rsn.endsWith("B")) {
-					return digits + RID_ENDS_WITH_B;
-				}
-			}
-			throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute);
-		}
-		return Long.parseLong(rsn); // using route short name as route ID
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
 	}
 
 	@NotNull
 	@Override
-	public String getRouteLongName(@NotNull GRoute gRoute) {
-		String routeLongName = gRoute.getRouteLongNameOrDefault();
+	public String cleanRouteLongName(@NotNull String routeLongName) {
 		routeLongName = CleanUtils.cleanSlashes(routeLongName);
 		return CleanUtils.cleanLabel(routeLongName);
 	}
 
-	@Nullable
 	@Override
-	public String getRouteShortName(@NotNull GRoute gRoute) {
-		return gRoute.getRouteShortName().trim();
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
 	private static final String AGENCY_COLOR_BROWN = "A19153"; // BROWN (from old logo)
@@ -131,66 +87,60 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 		return AGENCY_COLOR;
 	}
 
+	@Nullable
+	@Override
+	public String fixColor(@Nullable String color) {
+		if ("FF0000".equalsIgnoreCase(color)) { // RED
+			return null;
+		}
+		return super.fixColor(color);
+	}
+
 	@SuppressWarnings("DuplicateBranchesInSwitch")
 	@Nullable
 	@Override
-	public String getRouteColor(@NotNull GRoute gRoute) {
-		String routeColor = gRoute.getRouteColor();
-		if ("FF0000".equalsIgnoreCase(routeColor)) {
-			routeColor = null;
-		}
-		if (StringUtils.isEmpty(routeColor)) {
-			String rsnS = gRoute.getRouteShortName().trim();
-			if (!Utils.isDigitsOnly(rsnS)) {
-				if ("3A".equalsIgnoreCase(rsnS)) {
-					return "8FC74A"; // same as 3
-				} else if ("3B".equalsIgnoreCase(rsnS)) {
-					return "8FC74A"; // same as 3
-				}
-				throw new MTLog.Fatal("Unexpected route color %s!", gRoute);
+	public String provideMissingRouteColor(@NotNull GRoute gRoute) {
+		final String rsnS = gRoute.getRouteShortName().trim();
+		if (!CharUtils.isDigitsOnly(rsnS)) {
+			if ("3A".equalsIgnoreCase(rsnS)) {
+				return "8FC74A"; // same as 3
+			} else if ("3B".equalsIgnoreCase(rsnS)) {
+				return "8FC74A"; // same as 3
 			}
-			int rsn = Integer.parseInt(rsnS);
-			switch (rsn) {
-			// @formatter:off
-			case 1: return "F6863C";
-			case 2: return "26A450";
-			case 3: return "8FC74A";
-			case 5: return "F7ACB0";
-			case 6: return "933D40";
-			case 9: return "691A5C";
-			case 10: return "8F44AD";
-			case 11: return "3E4095";
-			case 12: return "00BEF2";
-			case 13: return "068C83";
-			case 14: return "1A5B33";
-			case 15: return "C4393C";
-			case 16: return "691A5C";
-			case 17: return "9D6743";
-			case 18: return "467C96";
-			case 19: return "ED258F";
-			case 20: return "FFCC2C";
-			case 21: return "ADA425";
-			case 22: return "D6400E";
-			case 23: return "A6787A";
-			case 24: return "363435";
-			case 25: return "3E4095";
-			case 26: return "363435";
-			// case 27: return null; // TO DO
-			case 30: return "EECE20";
-			// @formatter:on
-			default:
-				throw new MTLog.Fatal("Unexpected route color %s!", gRoute);
-			}
+			throw new MTLog.Fatal("Unexpected route color %s!", gRoute);
 		}
-		return super.getRouteColor(gRoute);
-	}
-
-	@Override
-	public void setTripHeadsign(@NotNull MRoute mRoute, @NotNull MTrip mTrip, @NotNull GTrip gTrip, @NotNull GSpec gtfs) {
-		mTrip.setHeadsignString(
-				cleanTripHeadsign(gTrip.getTripHeadsignOrDefault()),
-				gTrip.getDirectionIdOrDefault()
-		);
+		final int rsn = Integer.parseInt(rsnS);
+		switch (rsn) {
+		// @formatter:off
+		case 1: return "F6863C";
+		case 2: return "26A450";
+		case 3: return "8FC74A";
+		case 5: return "F7ACB0";
+		case 6: return "933D40";
+		case 9: return "691A5C";
+		case 10: return "8F44AD";
+		case 11: return "3E4095";
+		case 12: return "00BEF2";
+		case 13: return "068C83";
+		case 14: return "1A5B33";
+		case 15: return "C4393C";
+		case 16: return "691A5C";
+		case 17: return "9D6743";
+		case 18: return "467C96";
+		case 19: return "ED258F";
+		case 20: return "FFCC2C";
+		case 21: return "ADA425";
+		case 22: return "D6400E";
+		case 23: return "A6787A";
+		case 24: return "363435";
+		case 25: return "3E4095";
+		case 26: return "363435";
+		// case 27: return null; // TO DO
+		case 30: return "EECE20";
+		// @formatter:on
+		default:
+			throw new MTLog.Fatal("Unexpected route color %s!", gRoute);
+		}
 	}
 
 	@Override
@@ -216,11 +166,6 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
-	@Override
-	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		throw new MTLog.Fatal("Unexpected trips to merge: %s & %s!", mTrip, mTripToMerge);
-	}
-
 	private String[] getIgnoredWords() {
 		return new String[]{
 				"AL", "CBC", "CNIB", "EMCO", "HMP", "HSC", "JB", "JJ", "MFRC", "MUN", "MVR", "NL", "RDM", "RCMP", "RNC", "YMCA",
@@ -236,7 +181,7 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 	@NotNull
 	@Override
 	public String cleanStopName(@NotNull String gStopName) {
-		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName, getIgnoredWords());
+		gStopName = CleanUtils.toLowerCaseUpperCaseWords(getFirstLanguageNN(), gStopName, getIgnoredWords());
 		gStopName = _DASH_.matcher(gStopName).replaceAll(_DASH_REPLACEMENT);
 		gStopName = NUMBER_SIGN_.matcher(gStopName).replaceAll(NUMBER_SIGN_REPLACEMENT);
 		gStopName = CleanUtils.CLEAN_AND.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
@@ -248,16 +193,14 @@ public class StJohnSMetrobusTransitBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(gStopName);
 	}
 
-	private static final Pattern DIGITS = Pattern.compile("[\\d]+");
-
 	@Override
 	public int getStopId(@NotNull GStop gStop) {
 		//noinspection deprecation
 		final String stopId = gStop.getStopId();
-		if (Utils.isDigitsOnly(stopId)) {
+		if (CharUtils.isDigitsOnly(stopId)) {
 			return Integer.parseInt(stopId);
 		}
-		Matcher matcher = DIGITS.matcher(stopId);
+		final Matcher matcher = DIGITS.matcher(stopId);
 		if (matcher.find()) {
 			return Integer.parseInt(matcher.group());
 		}
